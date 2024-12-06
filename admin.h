132b57ac0ede6a4849a784c73b2cce024fdc2360
Diego Vásquez <101302838+DiegVas@users.noreply.github.com>
2024-12-06 00:05:53 -0600

    Creacion del primer proyecto de Estrucutura de datos en vacaciones del segundo semestre usac

---
+++ admin.h
@@ -0,0 +1,8 @@
+//
+// Created by HP on 06/12/2024.
+//
+
+#ifndef ADMIN_H
+#define ADMIN_H
+
+#endif //ADMIN_H
---
+++ main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+
+void showMenu();
+void login();
+void registerUser();
+
+// TODO : Credenciales de usuario admin
+const std::string ADMIN_USER = "admin";
+const std::string ADMIN_PASS = "admin";
+
+int main() {
+
+    int choice;
+
+    do {
+        showMenu();
+        std::cin >> choice;
+
+        switch (choice) {
+            case 1:
+                login();
+                break;
+            case 2:
+                std::cout << "Gracias por preferirnos!." << std::endl;
+                break;
+            default:
+                std::cout << "Por favor ingrese una opcion valida" << std::endl;
+        }
+
+    } while (choice != 2);
+}
+
+void showMenu() {
+    std::cout << "//////////////// RENTA DE ACTIVOS ////////////////" << std::endl;
+    std::cout << "[1]. Login" << std::endl;
+    std::cout << "[2]. Exit" << std::endl;
+    std::cout << "Ingrese una opcion:  ";
+}
+
+void login() {
+    std::cout << "*-------------------* LOGIN *-------------------*" << std::endl;
+    std::string username, password;
+    std::cout << "Ingrese su usuario: ";
+    std::cin >> username;
+    std::cout << "Ingrese la contraseña: ";
+    std::cin >> password;
+
+    if (username == ADMIN_USER && password == ADMIN_PASS) {
+        std::cout << "Bienvenido " << username << "!" << std::endl;
+    }
+}
+
+
